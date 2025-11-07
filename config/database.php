ecac6aff27ea8c3522cab5f55630cac05e842f7e
Harsh <dynaman456@email.com>
2025-11-07 20:07:14 +0530

    added env to env example

--- config/database.php
+++ config/database.php
@@ -35,6 +35,14 @@ return [
             'driver' => 'array',
         ],
 
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
     ],
 
     /*
